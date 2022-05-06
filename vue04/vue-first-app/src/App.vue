b2b6d6d5a6e315ab81285136476a11a4ad7fecdb
Su-ho Park <psuhoo122065@gmail.com>
2022-05-06 14:33:33 +0900

    Updated on May 06 2022 14:33

--- vue04/vue-first-app/src/App.vue
+++ vue04/vue-first-app/src/App.vue
@@ -1,19 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div>
+      <!-- 메시지 수신 -->
+      <h4>About Component Message</h4>
+      <AboutView :msg="msg" @throw-msg="writeMsg" />
+      {{ result }}
+      <hr>
+    </div>
+    <div>
+      <!-- 동적 바인딩 -->
+      <input type="text" name="" id="" v-model="msg" />
+    </div>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from "./components/HelloWorld.vue";
+import AboutView from "./components/AboutView.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,
+    AboutView,
+  },
+  data() {
+    return {
+      msg: "",
+      result: "",
+    };
+  },
+  methods: {
+    writeMsg(val) {
+      this.result = val;
+    },
+  },
+};
 </script>
 
 <style>
---
+++ vue04/vue-first-app/src/components/AboutView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h3>About Component</h3>
+    {{ msg }}
+    <h4>App.vue로 메시지 전송</h4>
+    <input type="text" name="" id="" v-model="text" />
+    <button @click="throwMsg">메시지 전송</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AboutView",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      text: "",
+    };
+  },
+  methods: {
+    throwMsg() {
+      this.$emit("throw-msg", this.text);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
