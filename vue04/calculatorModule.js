92ed961cee734b352461b2b609e7adb9035e0ea4
Su-ho Park <psuhoo122065@gmail.com>
2022-05-06 10:22:14 +0900

    Updated on May 06 2022 10:22

---
+++ vue04/calculatorModule.js
@@ -0,0 +1,17 @@
+export const title = 'Calculator';
+
+export function add(i, j) {
+    return i + j;
+}
+
+export function sub(i, j) {
+    return i - j;
+}
+
+export function mul(i, j) {
+    return i * j;
+}
+
+export function div(i, j) {
+    return i / j;
+}
---
+++ vue04/calculatorModule2.js
@@ -0,0 +1,11 @@
+export const title = 'Calculator';
+
+export default {
+    title: 'Calculator',
+    add(i, j){
+        return i + j;
+    },
+    sub(i, j){
+        return i - j;
+    }
+}
