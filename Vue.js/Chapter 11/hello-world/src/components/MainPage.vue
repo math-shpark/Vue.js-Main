c1aac7de076b85387bf47c63060c702b6d6913a9
Su-ho Park <psuhoo122065@gmail.com>
2022-05-15 00:18:02 +0900

    Updated on May 15 2022 00:17

---
+++ Vue.js/Chapter 11/hello-world/src/components/MainPage.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h3>Hello World!</h3>
+    <div class="sub" v-for="lang in langs" v-bind:key="lang.name">
+      <p>강의 언어 : {{ lang.name }}</p>
+      <p>강사명 : {{ lang.teacher }}</p>
+      <p>강의 시간 : {{ lang.time }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainPage",
+  data() {
+    return {
+      langs: [
+        {
+          name: "Vue.js",
+          teacher: "Hong",
+          time: 30,
+        },
+        {
+          name: "JavaScript",
+          teacher: "Lee",
+          time: 40,
+        },
+        {
+          name: "CSS",
+          teacher: "Kim",
+          time: 50,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.sub {
+  margin-left: 200px;
+  margin-right: 200px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  border: 2px solid;
+}
+</style>
---
+++ Vue.js/Chapter 12/test-project/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">값 : {{ getNum() }}</div>
+</template>
+
+<script>
+import sum from "../../index.js";
+
+export default {
+  name: "App",
+  methods: {
+    getNum() {
+      return sum(1, 1);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
